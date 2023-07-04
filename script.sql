61610d1bfc5b725bf6f1c1bea5c498dc7215d263
Paullette01 <2030215@upv.edu.mx>
2023-07-03 22:48:57 -0600

    script

---
+++ script.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS gestorProyectos;
+
+USE gestorProyectos;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    email_verified_at TIMESTAMP NULL,
+    password VARCHAR(255) NOT NULL,
+    remember_token VARCHAR(100) NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL
+);
